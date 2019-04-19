2c7f5df5daf26b8cf546cd489028c0f7ed748f98
Ben Grabowski <gbowski30@gmail.com>
2019-04-19 11:12:59 -0600

    added questions to index.js

---
+++ index.js
@@ -0,0 +1,91 @@
+const STORE = [
+    {
+      question: 'How many clubs is a player allowed to have in their bag?',
+      answer1: '10',
+      answer2: '14',
+      answer3: '12',
+      answer4: '15',
+      correctAnswer: '14'
+    },
+
+    {
+        question: 'Which golfer has won the most Major Championships in their career?',
+        answer1: 'Tiger Woods',
+        answer2: 'Jack Nicklaus',
+        answer3: 'Arnold Palmer',
+        answer4: 'Phil Mickelson',
+        correctAnswer: 'Jack Nicklaus'
+      },
+
+      {
+        question: 'What is it called when someone plays a hole in 2 strokes under par?',
+        answer1: 'Bogey',
+        answer2: 'Birdie',
+        answer3: 'Double-Birdie',
+        answer4: 'Eagle',
+        correctAnswer: 'Eagle'
+      },
+
+      {
+        question: 'How many holes are played during a typical PGA Tournament?',
+        answer1: '36',
+        answer2: '54',
+        answer3: '18',
+        answer4: '72',
+        correctAnswer: '72'
+      },
+
+      {
+        question: 'What is the diameter of a golf ball?',
+        answer1: '2"',
+        answer2: '1.68"',
+        answer3: '1.52"',
+        answer4: '1.73"',
+        correctAnswer: '1.68'
+      },
+
+      {
+        question: 'Which of these tournaments is not a Major Championship?',
+        answer1: 'The PGA Championship',
+        answer2: 'The Masters',
+        answer3: 'The Players Championship',
+        answer4: 'The U.S. Open',
+        correctAnswer: 'The Players Championship'
+      },
+
+      {
+        question: 'Where did Golf originate?',
+        answer1: 'Ireland',
+        answer2: 'England',
+        answer3: 'Scotland',
+        answer4: 'The United States',
+        correctAnswer: 'Scotland'
+      },
+
+      {
+        question: 'What is it called when a right-handed player hits a shot that curves right?',
+        answer1: 'Hook',
+        answer2: 'Draw',
+        answer3: 'Slice',
+        answer4: 'Blade',
+        correctAnswer: 'Slice'
+      },
+
+      {
+        question: 'Which of these clubs is the shortest?',
+        answer1: '4 iron',
+        answer2: 'Sand Wedge',
+        answer3: '8 iron',
+        answer4: 'Fairway Wood',
+        correctAnswer: 'Sand Wedge'
+      },
+
+      {
+        question: 'Where does the Masters take place?',
+        answer1: 'Augusta, ME',
+        answer2: 'Tampa, FL',
+        answer3: 'Hartford, CT',
+        answer4: 'Augusta, GA',
+        correctAnswer: 'Augusta, GA'
+      },
+]
